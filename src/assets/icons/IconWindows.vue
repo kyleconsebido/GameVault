f60374f8c50442cc6db10b32911b2913bd96440c
kyleconsebido <123555531+kyleconsebido@users.noreply.github.com>
2024-03-25 22:29:18 +0800

    Add Assets (#3)
    * add favicon
    
    * add logo.svg
    
    * remove boilerplate css & add color palette and base styles
    
    * change document title to Game Vault
    
    * add icons
    
    add platform icons (browser & windows)
    
    add barrel file for icons
    
    fixed incorrect logo
    
    updated windows icon

---
+++ src/assets/icons/IconWindows.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    v-bind="$attrs"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0.5 8.92798H5.84711V14.0993L0.5 13.2673V8.92798Z" stroke="white" />
+    <path d="M0.5 7.07202H5.84711L5.84711 1.9007L0.5 2.73275L0.5 7.07202Z" stroke="white" />
+    <path d="M7.77271 8.92798H15.5V15.4232L7.77271 14.3155V8.92798Z" stroke="white" />
+    <path d="M7.77271 7.07202H15.5V0.57679L7.77271 1.68447V7.07202Z" stroke="white" />
+  </svg>
+</template>
