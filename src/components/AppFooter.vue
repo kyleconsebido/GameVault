f626a5d1b5fc1c3e895cad507f72a5dcf477f67b
kyleconsebido <123555531+kyleconsebido@users.noreply.github.com>
2024-03-28 21:07:23 +0800

    add github link and license to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,20 @@
 <template>
   <footer>
     <div class="app-container">
-      <span class="footer-item">Copyright © 2024</span>
+      <span class="footer-item">
+        <a target="_blank" href="https://github.com/kyleconsebido/ITELEC4C-GameVault">
+          <img src="/github-mark-white.svg" class="logo" />
+        </a>
+        <span>
+          Copyright © 2024 &nbsp;-
+          <a
+            target="_blank"
+            href="https://github.com/kyleconsebido/ITELEC4C-GameVault/blob/master/LICENSE"
+          >
+            MIT License
+          </a>
+        </span>
+      </span>
       <span class="footer-item free-to-game">
         <span>Powered by FreeToGame</span>
         <a target="_blank" href="https://www.freetogame.com/">
@@ -24,23 +37,38 @@ footer {
   color: var(--color-text-dark);
 }
 
+a {
+  margin: none;
+  text-decoration: none;
+  color: var(--color-text-dark);
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
 .app-container {
   display: flex;
 }
 
 .logo {
-  width: 2rem;
-  height: 2rem;
+  width: 1.5em;
+  height: 1.5em;
 }
 
 a,
 .footer-item {
-  display: flex;
+  display: inline-flex;
   align-items: center;
   gap: 1em;
 }
 
 .free-to-game {
   margin-left: auto;
+}
+
+.free-to-game .logo {
+  width: 2em;
+  height: 2em;
 }
 </style>
